4af313a418baaa2dfd99bcfdae2cd2ee53e372c0
Demiurgo443 <43414189+Demiurgo443@users.noreply.github.com>
2020-08-01 19:33:01 +0200

    Fixed test_set_activity()

--- tests/test_case.py
+++ tests/test_case.py
@@ -27,15 +27,12 @@ def test_no_value():
 
 def test_set_activity():
     activity = Activity('activity1', datetime.strptime('2015/05/10 08:22:53.000', '%Y/%m/%d %H:%M:%S.%f'))
-    activity1 = Activity('activity 1', datetime.strptime('2015/05/10 08:22:53.000', '%Y/%m/%d %H:%M:%S.%f'))
     case = Case('2')
     case.set_activity(activity.name, activity.timestamp)
-    case.set_activity(activity1.name, activity1.timestamp)
 
     assert isinstance(case.activities[0], Activity)
     assert case.activities[0].name == 'activity1'
     assert case.activities[0].timestamp == datetime(2015, 5, 10, 8, 22, 53)
-    assert case.activities[1].name == 'activity_1'
 
 
 def test_last_time():
