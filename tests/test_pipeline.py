f7bfe8ab1909f83b8488645bdba53b2855f1d226
Demiurgo443 <43414189+Demiurgo443@users.noreply.github.com>
2020-08-20 22:20:08 +0200

    Updating _init_.py to ensure an easier user interface

--- tests/test_pipeline.py
+++ tests/test_pipeline.py
@@ -4,17 +4,17 @@ from cdesf2.core import CDESF
 
 def test_pipeline():
     path = 'demo'
-    filename = 'sudden_trace_noise20_500_cb.csv'
+    filename = 'hospital_billing.csv'
 
     event_stream_test = read_csv(f'{path}/{filename}')
-    cdesf = CDESF(name='sudden_trace_noise20_500_cb',
-                  time_horizon=259200,
+    cdesf = CDESF(name='hospital_billing',
+                  time_horizon=432000,
                   lambda_=0.05,
                   beta=0.2,
-                  epsilon=0.6,
+                  epsilon=0.2,
                   mu=4,
                   stream_speed=100,
                   n_features=2,
                   gen_metrics=True,
-                  gen_plot=False)
+                  gen_plot=True)
     cdesf.run(event_stream_test)
