3ba0dab37c1bfdb54fa87f7055de07acfa2e122e
Emilio Schepis <emilio.schepis@gmail.com>
2021-05-07 16:56:01 +0000

    feat: calculate distance for additional attributes

--- cdesf2/utils/extract_case_distances.py
+++ cdesf2/utils/extract_case_distances.py
@@ -1,11 +1,14 @@
 from math import log10
 import networkx as nx
 from typing import Tuple
+from numpy import average
 from ..data_structures import Case
 from .time_difference import time_difference
 
 
-def extract_case_distances(graph: nx.DiGraph, case: Case) -> "dict[str, float]":
+def extract_case_distances(
+    graph: nx.DiGraph, case: Case, additional_attributes: "list[str]" = []
+) -> "dict[str, float]":
     trace = case.get_trace()
     timestamps = case.get_timestamps()
     timestamp_differences = time_difference([timestamps])[0]
@@ -33,21 +36,43 @@ def extract_case_distances(graph: nx.DiGraph, case: Case) -> "dict[str, float]":
         # trace has only one edge, graph distance is the sum
         graph_distance = trace_weight
 
+    distances = {}
+    distances["graph"] = graph_distance
+
+    for attribute_name in additional_attributes:
+        attribute_diffs = 0
+        total_avg = 0
+
+        for index, activity in enumerate(trace):
+            # Take the current from the graph node's attributes
+            current_values = graph.nodes[activity][attribute_name]
+            total_avg += average(current_values)
+            attribute_diffs += abs(
+                average(current_values) - case.get_attribute(attribute_name)[index]
+            )
+
+        distances[attribute_name] = attribute_diffs / total_avg
+
     difference = 0
     time = time_difference([case.get_timestamps()])[0]
     for i in range(len(graph_time)):
         difference += abs(graph_time[i] - time[i])
-    if difference == 0:
-        # trace and graph have the exact same time distribution
-        return {"graph": graph_distance, "time": 0}
 
     graph_time_sum = sum(graph_time)
-    if graph_time_sum == 0:
+
+    if difference == 0:
+        # trace and graph have the exact same time distribution
+        time_distance = 0
+    elif graph_time_sum == 0:
         # graph time sum is 0, returns the log of the difference
-        return {"graph": graph_distance, "time": log10(difference)}
+        time_distance = log10(difference)
     else:
         # graph time sum and difference are not zero, original equation performed (most likely)
-        return {"graph": graph_distance, "time": log10(difference / graph_time_sum)}
+        time_distance = log10(difference / graph_time_sum)
+
+    distances["time"] = time_distance
+
+    return distances
 
 
 # def OLD_extract_case_distances(graph: nx.DiGraph, case: Case) -> Tuple[float, float]:
--- tests/test_extract_case_distances.py
+++ tests/test_extract_case_distances.py
@@ -5,293 +5,420 @@ from cdesf2.utils import initialize_graph, extract_case_distances
 import pytest
 
 
-def test_extract_case_distances():
-    case_list = []
+class TestExtractCase:
+    @pytest.fixture
+    def simple_graph_with_attributes(self):
+        case_list = []
 
-    case = Case("1")
-    case.add_event(
-        {
-            "concept:name": "Activity A",
-            "time:timestamp": datetime(2015, 5, 10, 8, 00, 00),
-        }
-    )
-    case.add_event(
-        {
-            "concept:name": "Activity D",
-            "time:timestamp": datetime(2015, 5, 10, 8, 33, 20),
-        }
-    )
-    case.add_event(
-        {
-            "concept:name": "Activity E",
-            "time:timestamp": datetime(2015, 5, 10, 14, 6, 40),
-        }
-    )
-    case_list.append(case)
+        case = Case("1")
+        case.add_event(
+            {
+                "concept:name": "activityA",
+                "time:timestamp": datetime(2015, 5, 10, 8, 00, 00),
+                "attribute_one": 10,
+                "attribute_two": 5,
+            }
+        )
+        case.add_event(
+            {
+                "concept:name": "activityB",
+                "time:timestamp": datetime(2015, 5, 10, 8, 00, 10),
+                "attribute_one": 0,
+                "attribute_two": 7,
+            }
+        )
+        case.add_event(
+            {
+                "concept:name": "activityC",
+                "time:timestamp": datetime(2015, 5, 10, 8, 00, 20),
+                "attribute_one": 2,
+                "attribute_two": 4,
+            }
+        )
+        case_list.append(case)
 
-    case = Case("2")
-    case.add_event(
-        {
-            "concept:name": "Activity A",
-            "time:timestamp": datetime(2015, 5, 10, 1, 00, 00),
-        }
-    )
-    case.add_event(
-        {
-            "concept:name": "Activity B",
-            "time:timestamp": datetime(2015, 5, 10, 14, 40, 00),
-        }
-    )
-    case.add_event(
-        {
-            "concept:name": "Activity D",
-            "time:timestamp": datetime(2015, 5, 10, 15, 5, 00),
-        }
-    )
-    case_list.append(case)
+        case = Case("2")
+        case.add_event(
+            {
+                "concept:name": "activityA",
+                "time:timestamp": datetime(2015, 5, 10, 8, 00, 00),
+                "attribute_one": 3,
+                "attribute_two": 10,
+            }
+        )
+        case.add_event(
+            {
+                "concept:name": "activityB",
+                "time:timestamp": datetime(2015, 5, 10, 8, 00, 10),
+                "attribute_one": 6,
+                "attribute_two": 2,
+            }
+        )
+        case_list.append(case)
 
-    case = Case("3")
-    case.add_event(
-        {
-            "concept:name": "Activity A",
-            "time:timestamp": datetime(2015, 5, 10, 8, 00, 00),
-        }
-    )
-    case.add_event(
-        {
-            "concept:name": "Activity B",
-            "time:timestamp": datetime(2015, 5, 10, 8, 13, 00),
-        }
-    )
-    case.add_event(
-        {
-            "concept:name": "Activity C",
-            "time:timestamp": datetime(2015, 5, 10, 8, 13, 00),
-        }
-    )
-    case_list.append(case)
+        case = Case("3")
+        case.add_event(
+            {
+                "concept:name": "activityA",
+                "time:timestamp": datetime(2015, 5, 10, 8, 00, 00),
+                "attribute_one": 8,
+                "attribute_two": 4,
+            }
+        )
+        case.add_event(
+            {
+                "concept:name": "activityB",
+                "time:timestamp": datetime(2015, 5, 10, 8, 00, 10),
+                "attribute_one": 1,
+                "attribute_two": 12,
+            }
+        )
+        case.add_event(
+            {
+                "concept:name": "activityC",
+                "time:timestamp": datetime(2015, 5, 10, 8, 00, 20),
+                "attribute_one": 2,
+                "attribute_two": 2,
+            }
+        )
+        case.add_event(
+            {
+                "concept:name": "activityD",
+                "time:timestamp": datetime(2015, 5, 10, 8, 00, 30),
+                "attribute_one": 1,
+                "attribute_two": 1,
+            }
+        )
+        case_list.append(case)
 
-    case = Case("4")
-    case.add_event(
-        {
-            "concept:name": "Activity A",
-            "time:timestamp": datetime(2015, 5, 10, 8, 00, 00),
-        }
-    )
-    case.add_event(
-        {
-            "concept:name": "Activity B",
-            "time:timestamp": datetime(2015, 5, 10, 8, 00, 20),
-        }
-    )
-    case.add_event(
-        {
-            "concept:name": "Activity C",
-            "time:timestamp": datetime(2015, 5, 10, 8, 00, 20),
-        }
-    )
-    case_list.append(case)
+        graph = nx.DiGraph(name="simple_graph_with_attributes")
+        graph = initialize_graph(graph, case_list, ["attribute_one", "attribute_two"])
 
-    case = Case("5")
-    case.add_event(
-        {
-            "concept:name": "Activity A",
-            "time:timestamp": datetime(2015, 5, 10, 8, 00, 00),
-        }
-    )
-    case.add_event(
-        {
-            "concept:name": "Activity B",
-            "time:timestamp": datetime(2015, 5, 10, 8, 00, 00),
-        }
-    )
-    case.add_event(
-        {
-            "concept:name": "Activity C",
-            "time:timestamp": datetime(2015, 5, 10, 8, 00, 00),
-        }
-    )
-    case_list.append(case)
+        return graph
 
-    case = Case("6")
-    case.add_event(
-        {
-            "concept:name": "Activity A",
-            "time:timestamp": datetime(2015, 5, 10, 8, 00, 00),
-        }
-    )
-    case.add_event(
-        {
-            "concept:name": "Activity B",
-            "time:timestamp": datetime(2015, 5, 10, 8, 00, 00),
-        }
-    )
-    case.add_event(
-        {
-            "concept:name": "Activity C",
-            "time:timestamp": datetime(2015, 5, 10, 15, 30, 00),
-        }
-    )
-    case.add_event(
-        {
-            "concept:name": "Activity F",
-            "time:timestamp": datetime(2015, 5, 10, 15, 30, 00),
-        }
-    )
-    case_list.append(case)
+    def test_extract_case_distances(self):
+        case_list = []
 
-    case = Case("7")
-    case.add_event(
-        {
-            "concept:name": "Activity A",
-            "time:timestamp": datetime(2015, 5, 10, 8, 00, 00),
-        }
-    )
-    case.add_event(
-        {
-            "concept:name": "Activity B",
-            "time:timestamp": datetime(2015, 5, 10, 8, 00, 00),
-        }
-    )
-    case.add_event(
-        {
-            "concept:name": "Activity C",
-            "time:timestamp": datetime(2015, 5, 10, 8, 00, 00),
-        }
-    )
-    case.add_event(
-        {
-            "concept:name": "Activity F",
-            "time:timestamp": datetime(2015, 5, 11, 8, 00, 00),
-        }
-    )
-    case_list.append(case)
+        case = Case("1")
+        case.add_event(
+            {
+                "concept:name": "Activity A",
+                "time:timestamp": datetime(2015, 5, 10, 8, 00, 00),
+            }
+        )
+        case.add_event(
+            {
+                "concept:name": "Activity D",
+                "time:timestamp": datetime(2015, 5, 10, 8, 33, 20),
+            }
+        )
+        case.add_event(
+            {
+                "concept:name": "Activity E",
+                "time:timestamp": datetime(2015, 5, 10, 14, 6, 40),
+            }
+        )
+        case_list.append(case)
 
-    case = Case("8")
-    case.add_event(
-        {
-            "concept:name": "Activity A",
-            "time:timestamp": datetime(2015, 5, 10, 8, 00, 00),
-        }
-    )
-    case.add_event(
-        {
-            "concept:name": "Activity B",
-            "time:timestamp": datetime(2015, 5, 10, 8, 00, 00),
-        }
-    )
-    case.add_event(
-        {
-            "concept:name": "Activity C",
-            "time:timestamp": datetime(2015, 5, 10, 8, 00, 00),
-        }
-    )
-    case.add_event(
-        {
-            "concept:name": "Activity F",
-            "time:timestamp": datetime(2015, 5, 10, 11, 46, 40),
-        }
-    )
-    case_list.append(case)
+        case = Case("2")
+        case.add_event(
+            {
+                "concept:name": "Activity A",
+                "time:timestamp": datetime(2015, 5, 10, 1, 00, 00),
+            }
+        )
+        case.add_event(
+            {
+                "concept:name": "Activity B",
+                "time:timestamp": datetime(2015, 5, 10, 14, 40, 00),
+            }
+        )
+        case.add_event(
+            {
+                "concept:name": "Activity D",
+                "time:timestamp": datetime(2015, 5, 10, 15, 5, 00),
+            }
+        )
+        case_list.append(case)
 
-    case = Case("9")
-    case.add_event(
-        {
-            "concept:name": "Activity A",
-            "time:timestamp": datetime(2015, 5, 10, 8, 00, 00),
-        }
-    )
-    case.add_event(
-        {
-            "concept:name": "Activity B",
-            "time:timestamp": datetime(2015, 5, 10, 8, 00, 00),
-        }
-    )
-    case.add_event(
-        {
-            "concept:name": "Activity C",
-            "time:timestamp": datetime(2015, 5, 10, 8, 00, 00),
-        }
-    )
-    case.add_event(
-        {
-            "concept:name": "Activity F",
-            "time:timestamp": datetime(2015, 5, 10, 8, 00, 00),
-        }
-    )
-    case_list.append(case)
+        case = Case("3")
+        case.add_event(
+            {
+                "concept:name": "Activity A",
+                "time:timestamp": datetime(2015, 5, 10, 8, 00, 00),
+            }
+        )
+        case.add_event(
+            {
+                "concept:name": "Activity B",
+                "time:timestamp": datetime(2015, 5, 10, 8, 13, 00),
+            }
+        )
+        case.add_event(
+            {
+                "concept:name": "Activity C",
+                "time:timestamp": datetime(2015, 5, 10, 8, 13, 00),
+            }
+        )
+        case_list.append(case)
 
-    case = Case("10")
-    case.add_event(
-        {
-            "concept:name": "Activity A",
-            "time:timestamp": datetime(2015, 5, 10, 8, 00, 00),
-        }
-    )
-    case.add_event(
-        {
-            "concept:name": "Activity B",
-            "time:timestamp": datetime(2015, 5, 10, 8, 00, 00),
-        }
-    )
-    case.add_event(
-        {
-            "concept:name": "Activity C",
-            "time:timestamp": datetime(2015, 5, 10, 8, 00, 00),
-        }
-    )
-    case.add_event(
-        {
-            "concept:name": "Activity F",
-            "time:timestamp": datetime(2015, 5, 10, 8, 00, 00),
-        }
-    )
-    case_list.append(case)
+        case = Case("4")
+        case.add_event(
+            {
+                "concept:name": "Activity A",
+                "time:timestamp": datetime(2015, 5, 10, 8, 00, 00),
+            }
+        )
+        case.add_event(
+            {
+                "concept:name": "Activity B",
+                "time:timestamp": datetime(2015, 5, 10, 8, 00, 20),
+            }
+        )
+        case.add_event(
+            {
+                "concept:name": "Activity C",
+                "time:timestamp": datetime(2015, 5, 10, 8, 00, 20),
+            }
+        )
+        case_list.append(case)
 
-    case = Case("11")
-    case.add_event(
-        {
-            "concept:name": "Activity A",
-            "time:timestamp": datetime(2015, 5, 10, 8, 00, 00),
-        }
-    )
-    case.add_event(
-        {
-            "concept:name": "Activity B",
-            "time:timestamp": datetime(2015, 5, 10, 8, 00, 00),
-        }
-    )
-    case.add_event(
-        {
-            "concept:name": "Activity C",
-            "time:timestamp": datetime(2015, 5, 10, 8, 00, 00),
-        }
-    )
-    case_list.append(case)
+        case = Case("5")
+        case.add_event(
+            {
+                "concept:name": "Activity A",
+                "time:timestamp": datetime(2015, 5, 10, 8, 00, 00),
+            }
+        )
+        case.add_event(
+            {
+                "concept:name": "Activity B",
+                "time:timestamp": datetime(2015, 5, 10, 8, 00, 00),
+            }
+        )
+        case.add_event(
+            {
+                "concept:name": "Activity C",
+                "time:timestamp": datetime(2015, 5, 10, 8, 00, 00),
+            }
+        )
+        case_list.append(case)
 
-    graph = nx.DiGraph()
-    graph = initialize_graph(graph, case_list)
+        case = Case("6")
+        case.add_event(
+            {
+                "concept:name": "Activity A",
+                "time:timestamp": datetime(2015, 5, 10, 8, 00, 00),
+            }
+        )
+        case.add_event(
+            {
+                "concept:name": "Activity B",
+                "time:timestamp": datetime(2015, 5, 10, 8, 00, 00),
+            }
+        )
+        case.add_event(
+            {
+                "concept:name": "Activity C",
+                "time:timestamp": datetime(2015, 5, 10, 15, 30, 00),
+            }
+        )
+        case.add_event(
+            {
+                "concept:name": "Activity F",
+                "time:timestamp": datetime(2015, 5, 10, 15, 30, 00),
+            }
+        )
+        case_list.append(case)
 
-    case = Case("12")
-    case.add_event(
-        {
-            "concept:name": "Activity A",
-            "time:timestamp": datetime(2015, 5, 10, 8, 00, 00),
-        }
-    )
-    case.add_event(
-        {
-            "concept:name": "Activity B",
-            "time:timestamp": datetime(2015, 5, 10, 9, 18, 20),
-        }
-    )
-    case.add_event(
-        {
-            "concept:name": "Activity C",
-            "time:timestamp": datetime(2015, 5, 10, 10, 8, 20),
-        }
-    )
+        case = Case("7")
+        case.add_event(
+            {
+                "concept:name": "Activity A",
+                "time:timestamp": datetime(2015, 5, 10, 8, 00, 00),
+            }
+        )
+        case.add_event(
+            {
+                "concept:name": "Activity B",
+                "time:timestamp": datetime(2015, 5, 10, 8, 00, 00),
+            }
+        )
+        case.add_event(
+            {
+                "concept:name": "Activity C",
+                "time:timestamp": datetime(2015, 5, 10, 8, 00, 00),
+            }
+        )
+        case.add_event(
+            {
+                "concept:name": "Activity F",
+                "time:timestamp": datetime(2015, 5, 11, 8, 00, 00),
+            }
+        )
+        case_list.append(case)
 
-    distances = extract_case_distances(graph, case)
-    assert distances.get("graph") == pytest.approx(0.05)
-    assert distances.get("time") == pytest.approx(-1.42, rel=1e-2)
+        case = Case("8")
+        case.add_event(
+            {
+                "concept:name": "Activity A",
+                "time:timestamp": datetime(2015, 5, 10, 8, 00, 00),
+            }
+        )
+        case.add_event(
+            {
+                "concept:name": "Activity B",
+                "time:timestamp": datetime(2015, 5, 10, 8, 00, 00),
+            }
+        )
+        case.add_event(
+            {
+                "concept:name": "Activity C",
+                "time:timestamp": datetime(2015, 5, 10, 8, 00, 00),
+            }
+        )
+        case.add_event(
+            {
+                "concept:name": "Activity F",
+                "time:timestamp": datetime(2015, 5, 10, 11, 46, 40),
+            }
+        )
+        case_list.append(case)
+
+        case = Case("9")
+        case.add_event(
+            {
+                "concept:name": "Activity A",
+                "time:timestamp": datetime(2015, 5, 10, 8, 00, 00),
+            }
+        )
+        case.add_event(
+            {
+                "concept:name": "Activity B",
+                "time:timestamp": datetime(2015, 5, 10, 8, 00, 00),
+            }
+        )
+        case.add_event(
+            {
+                "concept:name": "Activity C",
+                "time:timestamp": datetime(2015, 5, 10, 8, 00, 00),
+            }
+        )
+        case.add_event(
+            {
+                "concept:name": "Activity F",
+                "time:timestamp": datetime(2015, 5, 10, 8, 00, 00),
+            }
+        )
+        case_list.append(case)
+
+        case = Case("10")
+        case.add_event(
+            {
+                "concept:name": "Activity A",
+                "time:timestamp": datetime(2015, 5, 10, 8, 00, 00),
+            }
+        )
+        case.add_event(
+            {
+                "concept:name": "Activity B",
+                "time:timestamp": datetime(2015, 5, 10, 8, 00, 00),
+            }
+        )
+        case.add_event(
+            {
+                "concept:name": "Activity C",
+                "time:timestamp": datetime(2015, 5, 10, 8, 00, 00),
+            }
+        )
+        case.add_event(
+            {
+                "concept:name": "Activity F",
+                "time:timestamp": datetime(2015, 5, 10, 8, 00, 00),
+            }
+        )
+        case_list.append(case)
+
+        case = Case("11")
+        case.add_event(
+            {
+                "concept:name": "Activity A",
+                "time:timestamp": datetime(2015, 5, 10, 8, 00, 00),
+            }
+        )
+        case.add_event(
+            {
+                "concept:name": "Activity B",
+                "time:timestamp": datetime(2015, 5, 10, 8, 00, 00),
+            }
+        )
+        case.add_event(
+            {
+                "concept:name": "Activity C",
+                "time:timestamp": datetime(2015, 5, 10, 8, 00, 00),
+            }
+        )
+        case_list.append(case)
+
+        graph = nx.DiGraph()
+        graph = initialize_graph(graph, case_list)
+
+        case = Case("12")
+        case.add_event(
+            {
+                "concept:name": "Activity A",
+                "time:timestamp": datetime(2015, 5, 10, 8, 00, 00),
+            }
+        )
+        case.add_event(
+            {
+                "concept:name": "Activity B",
+                "time:timestamp": datetime(2015, 5, 10, 9, 18, 20),
+            }
+        )
+        case.add_event(
+            {
+                "concept:name": "Activity C",
+                "time:timestamp": datetime(2015, 5, 10, 10, 8, 20),
+            }
+        )
+
+        distances = extract_case_distances(graph, case)
+        assert distances.get("graph") == pytest.approx(0.05)
+        assert distances.get("time") == pytest.approx(-1.42, rel=1e-2)
+
+    def test_extract_case_distances_with_attributes(
+        self,
+        simple_graph_with_attributes: nx.DiGraph,
+    ):
+        case = Case("4")
+        case.add_event(
+            {
+                "concept:name": "activityA",
+                "time:timestamp": datetime(2015, 5, 10, 8, 00, 00),
+                "attribute_one": 9,
+                "attribute_two": 6,
+            }
+        )
+        case.add_event(
+            {
+                "concept:name": "activityB",
+                "time:timestamp": datetime(2015, 5, 10, 9, 18, 20),
+                "attribute_one": 5,
+                "attribute_two": 10,
+            }
+        )
+        case.add_event(
+            {
+                "concept:name": "activityC",
+                "time:timestamp": datetime(2015, 5, 10, 10, 8, 20),
+                "attribute_one": 2,
+                "attribute_two": 2,
+            }
+        )
+
+        distances = extract_case_distances(
+            simple_graph_with_attributes, case, ["attribute_one", "attribute_two"]
+        )
+        assert distances.get("attribute_one") == pytest.approx(0.4121, rel=1e-2)
+        assert distances.get("attribute_two") == pytest.approx(0.2651, rel=1e-2)
