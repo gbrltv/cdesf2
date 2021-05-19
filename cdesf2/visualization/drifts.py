b09a4124af768be4599a736d09195340fb26b9f1
Emilio Schepis <emilio.schepis@gmail.com>
2021-05-19 18:21:03 +0000

    fix: only plot drifts if any occurred

--- cdesf2/visualization/drifts.py
+++ cdesf2/visualization/drifts.py
@@ -16,15 +16,19 @@ def cumulative_stream_drifts(stream_length: int, drifts_index: List, save_path: 
     save_path: str
         Path for the file to be saved
     """
-    plt.figure(figsize=(15, 7.5))
-    for drift in drifts_index[:-1]:
-        plt.axvline(drift, ymin=0, ymax=1, linestyle='dashed', c='k', alpha=0.5)
-    plt.axvline(drifts_index[-1], ymin=0, ymax=1, linestyle='dashed', c='k', alpha=0.5, label='Drift points')
 
-    y_values = list(range(1, len(drifts_index)+1))
-    drifts_index.append(stream_length)
-    y_values.append(y_values[-1])
-    ax = sns.lineplot(x=drifts_index, y=y_values, linewidth=3, label='Number of drifts')
+    plt.figure(figsize=(15, 7.5))
+
+    if len(drifts_index) >= 1:
+        # Only plot if there is a drift.
+        for drift in drifts_index[:-1]:
+            plt.axvline(drift, ymin=0, ymax=1, linestyle='dashed', c='k', alpha=0.5)
+        plt.axvline(drifts_index[-1], ymin=0, ymax=1, linestyle='dashed', c='k', alpha=0.5, label='Drift points')
+
+        y_values = list(range(1, len(drifts_index)+1))
+        drifts_index.append(stream_length)
+        y_values.append(y_values[-1])
+        ax = sns.lineplot(x=drifts_index, y=y_values, linewidth=3, label='Number of drifts')
 
     plt.xlim(0, stream_length)
     plt.title("Drifts and cumulative number of drifts in the stream", size=20)
